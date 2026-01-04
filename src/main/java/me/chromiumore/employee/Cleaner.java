6682f49f6c0f917a6da5648e5f8d08f2bd8fab6d
Chromiumore <parts.dima06@gmail.com>
2026-01-05 01:35:56 +0300

    enclosure

---
+++ src/main/java/me/chromiumore/employee/Cleaner.java
@@ -0,0 +1,7 @@
+package me.chromiumore.employee;
+
+import me.chromiumore.enclosure.Enclosure;
+
+public interface Cleaner {
+    void clean(Enclosure enclosure);
+}
---
+++ src/main/java/me/chromiumore/enclosure/Enclosure.java
@@ -0,0 +1,47 @@
+package me.chromiumore.enclosure;
+
+import me.chromiumore.animal.Animal;
+import me.chromiumore.employee.Cleaner;
+import me.chromiumore.employee.Doctor;
+import me.chromiumore.employee.Feeder;
+
+import java.util.ArrayList;
+
+public class Enclosure {
+    private ArrayList<Animal> animals = new ArrayList<>();
+    private Doctor doctor;
+    private Feeder feeder;
+    private Cleaner cleaner;
+    private double purityLevel;
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+    public void increasePurity(double value) {
+        purityLevel += Math.abs(value);
+        purityLevel = Math.min(1, purityLevel);
+    }
+
+    public void reducePurity(double value) {
+        purityLevel -= Math.abs(value);
+        purityLevel = Math.max(purityLevel, 0);
+    }
+
+    public void startProcedures() {
+        increasePurity(0.65);
+
+        System.out.println("====== Начало процедур в вольере ======");
+
+        for (Animal a : animals) {
+            if (a.checkRequiresCare()) {
+                doctor.treatAnimal(a);
+                feeder.feedAnimal(a);
+            }
+        }
+
+        cleaner.clean(this);
+
+        System.out.println("====== Окончание процедур ======");
+    }
+}

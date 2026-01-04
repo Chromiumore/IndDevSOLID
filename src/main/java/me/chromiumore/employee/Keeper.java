3734ff1105d02052206452fd90bc0e1cbb2472dc
Chromiumore <parts.dima06@gmail.com>
2026-01-05 02:03:34 +0300

    keeper cleaner imp

--- src/main/java/me/chromiumore/employee/Keeper.java
+++ src/main/java/me/chromiumore/employee/Keeper.java
@@ -1,10 +1,11 @@
 package me.chromiumore.employee;
 
 import me.chromiumore.animal.Animal;
+import me.chromiumore.enclosure.Enclosure;
 
 import java.time.LocalDate;
 
-public class Keeper extends Employee implements Feeder {
+public class Keeper extends Employee implements Feeder, Cleaner {
     public Keeper(String name, int age, LocalDate hiredOn) {
         super(name, age, hiredOn);
     }
@@ -17,6 +18,12 @@ public class Keeper extends Employee implements Feeder {
     public void feedAnimal(Animal animal) {
         System.out.println(toString() + " кормит " + toString());
         animal.eat();
+    }
+
+    @Override
+    public void clean(Enclosure enclosure) {
+        System.out.println(toString() + " немного убирает вольер");
+        enclosure.reducePurity(0.6);
     }
 
     @Override
